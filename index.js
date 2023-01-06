f8fc91eb0dc04795b1f5ddefd4ec0b51f16e0d4f
Eduardo Henrique <eduardohenriq0612002@gmail.com>
2023-01-06 12:35:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var radios = document.getElementsByName('op');
+  for(radio in radios) {
+    radios[radio].onclick = function() {
+        switch(this.value){
+            case '1':
+                document.getElementById('cnt1').style.marginTop = '0';
+                break
+            case '2':
+                document.getElementById('cnt1').style.marginTop = '-65.25%';
+                break
+            case '3':
+                document.getElementById('cnt1').style.marginTop = '-130.5%';
+                break
+            case '4':
+                document.getElementById('cnt1').style.marginTop = "-195.5%"
+                
+        }
+    }
+}
